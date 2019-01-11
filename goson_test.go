e0f0aaef4649cfce98d1067fff09aee59a42225a
Eko Simanjuntak <ecojuntak@gmail.com>
2019-01-11 18:59:14 +0700

    chore: Add test coverage (#10)
    chore: Add test coverage

--- goson_test.go
+++ goson_test.go
@@ -31,6 +31,26 @@ var (
 		"nested":[[1,2], [2,4]],
 		"null": null
 	}`
+	malformedJSON = `{
+		"id": "uk123",
+		"name": "tom",
+		"email": "tom@gmail.com",
+		"user": {
+			"address": "Sidikalang"
+			"age": 23,
+			"phones": [
+				"08234239523",
+				"08234239523"
+			]
+		},
+		"arr": [{
+			"site": "string",
+			"url": "string"
+		}],
+		"tags": ["go", "js"],
+		"numbers": [3, 4, 5],
+		"nested":[[1,2], [2,4]] 
+	}`
 	exampleArr = `[{
 		"id": "uk123",
 		"name": "tom",
@@ -50,8 +70,29 @@ var (
 		"tags": ["go", "js"],
 		"numbers": [3, 4, 5] 
 	}]`
-	exampleArr2 = `[1 ,3 ,4 ,5]`
-	filePath    = "./testfile.json"
+	malformedArray = `[{
+		"id": "uk123",
+		"name": "tom",
+		"email": "tom@gmail.com",
+		"user": {
+			"address": "Sidikalang"
+			"age": 23,
+			"phones": [
+				"08234239523",
+				"08234239523"
+			]
+		},
+		"arr": [{
+			"site": "string",
+			"url": "string"
+		}],
+		"tags": ["go", "js"],
+		"numbers": [3, 4, 5] 
+	}]`
+	exampleArr2   = `[1 ,3 ,4 ,5]`
+	filePath      = "./testfile.json"
+	wrongFilePath = "./testdata/testfile.json"
+	wrongEndpoint = "test.qwerty.com:1234/post/1/a/1"
 )
 
 func TestJSONSchema(t *testing.T) {
@@ -64,6 +105,20 @@ func TestJSONSchema(t *testing.T) {
 		t.Error("Expected nil; but got: ", err)
 	}
 	fmt.Println("[SCHEMA] ", string(jbyt))
+}
+
+func TestJSONSchema_MalformedJSON(t *testing.T) {
+	_, err := goson.GenerateJSONSchema(malformedJSON)
+	if err == nil {
+		t.Error("Expected nil; but got: ", err)
+	}
+}
+
+func TestJSONSchema_MalformedArray(t *testing.T) {
+	_, err := goson.GenerateJSONSchema(malformedArray)
+	if err == nil {
+		t.Error("Expected nil; but got: ", err)
+	}
 }
 
 func TestJSONSchemaArray(t *testing.T) {
@@ -107,6 +162,13 @@ func TestJSONSchemaFromURL(t *testing.T) {
 	fmt.Println("[SCHEMA] ", string(jbyt))
 }
 
+func TestJSONSchemaFromURL_WrongEndpoint(t *testing.T) {
+	_, err := goson.GenerateJSONSchemaFromURL(wrongEndpoint)
+	if err == nil {
+		t.Error("Expected nil; but got: ", err)
+	}
+}
+
 func TestJSONSchemaFromFile(t *testing.T) {
 	schema, err := goson.GenerateJSONSchemaFromFile(filePath)
 	if err != nil {
@@ -117,4 +179,11 @@ func TestJSONSchemaFromFile(t *testing.T) {
 		t.Error("Expected nil; but got: ", err)
 	}
 	fmt.Println("[SCHEMA] ", string(jbyt))
+}
+
+func TestJSONSchemaFromFile_WrongPath(t *testing.T) {
+	_, err := goson.GenerateJSONSchemaFromFile(wrongFilePath)
+	if err == nil {
+		t.Error("Expected nil; but got: ", err)
+	}
 }
